aab28d45252ea4f08abad71e35de427ede486906
danniranderis <danni@danniranderis.dk>
2017-11-01 10:23:09 +0100

    Fixing up PEP8

--- binary_converter.py
+++ binary_converter.py
@@ -13,11 +13,13 @@ def binary_to_decimal(number):
     """ Converts a binary-number to decimal-number """
     return int(number, 2)
 
+
 def decimal_to_binary(number):
     """ Converts a decimal-number to its binary representation """
     # FIXME: Not the best way to do it - it's dirty, but works for now
     return bin(int(number))[2:]
- 
+
+
 def ipv4_to_binary(number):
     """ Converts a dotted IPv4 to its binary representation """
     binary_num = []
@@ -25,6 +27,7 @@ def ipv4_to_binary(number):
     for octet in number.split('.'):
         binary_num.append(decimal_to_binary(octet))
     return '.'.join(binary_num)
+
 
 def binary_ipv4_to_ipv4(number):
     """ Converts a dotted IPv4 in binary to its decimal representation """
@@ -37,17 +40,19 @@ def binary_ipv4_to_ipv4(number):
 
 def convert_file(file_path=None, input_type=None):
     """
-    Read a files lines and convert all decimalnumbers (one-per-line), then print the result
+    Read a files lines and convert all decimal-numbers (one-per-line), then
+    print the result.
     
     @param file_path: path to file for looping over
-    @param input_type: define the input format [dec|bin|ip] and output vise-versa
+    @param input_type: define the input format [dec|bin|ip] and output
+        vise-versa
     """
     with open(file_path) as f:
         for line in f:
             # Strip newline-chars
             line = line.strip('\n')
             
-            # FIXME: rewrite to use python classbased and extends
+            # FIXME: rewrite to use python class-based and extends
             # Create output
             if input_type == 'dec':
                 output_number = decimal_to_binary(line)
@@ -58,19 +63,21 @@ def convert_file(file_path=None, input_type=None):
             elif input_type == 'ip_bin':
                 output_number = binary_ipv4_to_ipv4(line)
             
-            print('{unconverted_number} = {output}'.format(unconverted_number=line,
-                                                           output=output_number))
+            print('{unconverted_number} = {output}'.format(
+                unconverted_number=line,
+                output=output_number))
 
 
 if __name__ == '__main__':
-    # Only if the file is called (not just imported), then do the convertion
+    # Only if the file is called (not just imported), then do the conversion
     
     # Extract args given the script at run
     try:
         file_path_arg = sys.argv[1]
         input_type_arg = sys.argv[2]
     except IndexError:
-        print('Du skal angive enten en sti til en fil der kan køres igennem som første argument')
+        print('Du skal angive enten en sti til en fil der kan køres igennem '
+              'som første argument')
 
     # Define if we only need to convert a single number, or a whole file
 
